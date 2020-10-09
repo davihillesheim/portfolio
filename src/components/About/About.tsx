ec2f4c49a20ac48f0260b131f7367993ddea416e
Davi Hillesheim <hillesheim@pop-os.localdomain>
2020-10-09 18:42:51 -0300

    minor changes in navbar and about

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className="about">
+      <div className="container">
+        <h5>About me</h5>
+      </div>
+    </div>
   );
 }
 
