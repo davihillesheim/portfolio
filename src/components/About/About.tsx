e4b0c5fad6afc08b1625f71312e468bab12bc50b
Davi Hillesheim <hillesheim@pop-os.localdomain>
2020-10-09 19:09:13 -0300

    add basic structure to about page

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
   return (
     <div className="about">
-      <div className="container">
-        <h5>About me</h5>
+      <div className="about-container">
+        <div className="text-formatter">
+          <h5>About me</h5>
+          <p className="about-paragraph"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique tristique suscipit. Suspendisse ac ligula sollicitudin, condimentum dui sit amet, dignissim augue. In convallis tortor vel tortor accumsan, interdum finibus libero sollicitudin. Aenean sit amet sem suscipit, vulputate arcu id, lacinia turpis. Nunc venenatis justo at pharetra porttitor. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus aliquet erat sodales consectetur mattis. Suspendisse potenti. Praesent interdum eleifend sodales. Nam sit amet erat id dolor posuere fermentum. Mauris convallis ante vel purus iaculis, sit amet posuere sapien volutpat.
+
+Curabitur sed mollis lectus, eget porta felis. Cras pretium sapien eget tellus euismod, quis interdum felis laoreet. Donec pretium lobortis turpis, ac pharetra diam iaculis nec. Morbi ipsum enim, posuere ut enim in, finibus rhoncus turpis. In arcu nunc, ultricies eget ex vel, luctus consequat augue. Donec eu maximus lacus. Curabitur ut suscipit massa. Suspendisse eu leo nisl. Nam dui urna, ultricies eu euismod id, imperdiet at nisl. Proin posuere lacus id turpis porta finibus. Nunc a purus ut leo vulputate pellentesque.
+
+Nulla porta tortor non eleifend ornare. Nulla dapibus venenatis est, vestibulum consequat nisl convallis nec. Duis tincidunt tincidunt semper. Maecenas at hendrerit diam. Nunc sodales, purus tincidunt dapibus hendrerit, mi lacus consequat justo, imperdiet blandit enim velit quis ipsum. Vivamus interdum sodales porta. Nullam eu neque ornare, dignissim urna in, eleifend nibh. Vestibulum finibus eros sit amet tortor vehicula tristique. Maecenas venenatis sapien quis nibh dapibus, ut dapibus orci maximus. Etiam a hendrerit velit, eget interdum mauris. Nulla accumsan nibh eget augue aliquam, quis scelerisque felis suscipit. Pellentesque ultricies sed odio consectetur suscipit. Proin sagittis condimentum urna vel tincidunt. Fusce pharetra massa nisl, vel aliquet tellus scelerisque nec. Donec ullamcorper lorem et laoreet dictum. Aenean magna velit, consequat nec libero ut, ultricies placerat lacus. 
+          </p>
+        </div>
       </div>
     </div>
   );
