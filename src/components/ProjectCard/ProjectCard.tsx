445d64bb27c8446bf363dccaf00a1fa9ab9d33fe
Davi Hillesheim <hillesheim@pop-os.localdomain>
2020-10-10 19:31:41 -0300

    add props to project card

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,22 +1,36 @@
 import React from 'react';
 
-const ProjectCard = () => {
+type CardProps = {
+    img: string,
+    title: string,
+    description: string,
+    tech: [string],
+    github: string,
+    external: string
+}
+
+const ProjectCard = ({ img, title, description, tech, github, external } : CardProps) => {
     return (
         <div className="project-card">
             <div className="project-content">
                 <h3 className="project-title">
-                    Projeto1
+                    {title}
                 </h3>
                 <div className="project-description">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-                Mauris mollis vitae augue quis sagittis. 
-                Donec at metus pharetra, dignissim felis a, ullamcorper felis. 
+                    <p>{description}</p> 
                 </div>
-                <ul className="project-tech-list"></ul>
-                <div className="project-links"></div>
+                <ul className="project-tech-list">
+                    {tech.map((tech, i) => (
+                        <li key={i}>{tech}</li>
+                    ))}
+                </ul>
+                <div className="project-links">
+                    <a href={github}>svg later</a>
+                    <a href={external}>svg later</a>
+                </div>
             </div>
             <div className="project-image">
-
+                <img alt="project page" src={img}/>
             </div>
         </div>
     );
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import ProjectCard from '../ProjectCard/ProjectCard';
 
 const Projects = () => {
   return (
     <div className="projects">
       <div className="projects-container">
-        
+        <h3>Projects</h3>
       </div>
     </div>
   );
